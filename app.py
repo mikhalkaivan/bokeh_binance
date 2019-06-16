a41ff2cd61ccdfb2d8fabfacd228f45d1c2192ab
Ivan Mikhalka <mikhalkaivan@mail.ru>
2019-06-17 00:38:03 +0300

    efef

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<int:bars_count>/')
+def chart(bars_count):
+    if bars_count <= 0:
+        bars_count = 1
+    return render_template('chart.html',bars_count=bars_count)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

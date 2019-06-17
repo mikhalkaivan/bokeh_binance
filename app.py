065c5d0feb5cf85875e01d7b39c839ea969bfaef
Ivan Mikhalka <mikhalkaivan@mail.ru>
2019-06-17 13:33:42 +0300

    dfsdf

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,request,jsonify
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/bokeh/')
+def chart():
+    fig = figure(plot_width=600,plot_height=600)
+    fig.vbar(x=[1, 2, 3, 4],width=0.5,bottom=0,top=[1.7, 2.2, 4.6, 3.9],color='navy')
 
+    #grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
-@app.route('/<int:bars_count>/')
-def chart(bars_count):
-    if bars_count <= 0:
-        bars_count = 1
-    return render_template('chart.html',bars_count=bars_count)
+    script,div = components(fig)
+    html = render_template('chart.html',plot_script=script,plot_div=div,js_resources=js_resources,css_resources=css_resources,)
+    return encode_utf8(html)
 
 
 
